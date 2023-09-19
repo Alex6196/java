e0da0a80aa60953c92dc511fe0d1683daec8702a
Om <omprakashshrestha6196@gmail.com>
2023-09-19 18:44:21 -0500

    Completed

--- src/DateTimeConverter.java
+++ src/DateTimeConverter.java
@@ -3,7 +3,6 @@ import java.text.ParseException;
 import java.util.Date;
 
 public class DateTimeConverter {
-//    retesting
     public static String convertToReadableTime(String datetimeStr){
         //defining date format for parsing the input datetime string
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
